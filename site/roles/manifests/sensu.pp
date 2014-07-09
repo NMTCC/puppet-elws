32e1d1977f2977fa90aa3483886eaa423a98704e
Louis <louisjencka@gmail.com>
2014-07-09 16:23:27 -0600

    Consolidate sensu commands

--- site/roles/manifests/sensu.pp
+++ site/roles/manifests/sensu.pp
@@ -1,11 +1,8 @@
 # class roles::sensu
 
 class roles::sensu {
-  
+  include profiles::sensu
   include profiles::sensu::plugins
-
-
-
-
-
+  
+  Class[profiles::sensu] -> Class[profiles::sensu::plugins]
 }

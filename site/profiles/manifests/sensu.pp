ad2a83361d0f3c9104e50018712fad2cd1e99f06
Louis <louisjencka@gmail.com>
2014-07-17 11:53:25 -0600

    Fix sensu class double definition

--- site/profiles/manifests/sensu.pp
+++ site/profiles/manifests/sensu.pp
@@ -1,7 +1,15 @@
 class profiles::sensu {
+  $rabbitmq_port            = 5671
+  $rabbitmq_password        = hiera(dbpasswd)
+  $rabbitmq_host            = 'mqhost.nmt.edu'
+  $subscriptions            = ['fedora',]
+  $repo_source              = 'http://repos.sensuapp.org/yum/el/6/$basearch/'
+  $rabbitmq_ssl_private_key = 'puppet:///files/sensu/ssl/key.pem'
+  $rabbitmq_ssl_cert_chain  = 'puppet:///files/sensu/ssl/cert.pem'
+
   package { 'sensu-gem':
     ensure    => 'latest',
     provider  => 'gem',
     name      => 'sensu-plugin',
-  } ->
+  }
 }

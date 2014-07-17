16758cab5a86a857cb0cd0cb175d72f44702af22
Louis <louisjencka@gmail.com>
2014-07-17 13:55:26 -0600

    Fix TCC role class paths

--- site/roles/manifests/tcc.pp
+++ site/roles/manifests/tcc.pp
@@ -4,14 +4,15 @@ class roles::tcc {
 
   include profiles::tcc
   include profiles::tcc::autofs
-  include profiles::tcc::config
+  include profiles::tcc::files::config
+  include profiles::tcc::files::script
   include profiles::tcc::yum
   include profiles::tcc::yum::packages
   include profiles::tcc::yum::localizations
   include profiles::tcc::ldap
   include profiles::tcc::passwd
   include profiles::tcc::remctl
-  include profiles::tcc::rsync::all
+  include profiles::rsync::all
 
   include profiles::iris
 

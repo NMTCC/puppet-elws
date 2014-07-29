173b1abdd25fcb6b01ed5196d0bdef3645fa39d6
Louis <louisjencka@gmail.com>
2014-07-29 12:54:02 -0600

    Consistency changes

--- site/profiles/manifests/puppetagent.pp
+++ site/profiles/manifests/puppetagent.pp
@@ -1,6 +1,6 @@
 class profiles::puppetagent {
-  exec { "puppetconf":
-      command => "puppet agent config server puppet-elws.nmt.edu",
+  exec { 'puppetconf':
+      command => 'puppet agent config server puppet-elws.nmt.edu',
       provider => shell
   }
   service { 'puppet':
